3d8cf99b5966c7710b80847473ee20f796a556a5
Colleen Rooney <crooney@pdx.edu>
2020-01-29 12:19:44 -0800

    add the logic

--- diamond/src/main/java/edu/pdx/cs410J/crooney/Diamond.java
+++ diamond/src/main/java/edu/pdx/cs410J/crooney/Diamond.java
@@ -1,5 +1,8 @@
 package edu.pdx.cs410J.crooney;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A class for getting started with a code kata
  *
@@ -14,7 +17,33 @@ public class Diamond {
     System.exit(1);
   }
 
-  public static String makeDiamond(char a) {
-    return "A";
+  public static List<String> makeDiamond(char inputChar) {
+    List<String> diamondLines = new ArrayList<>();
+    int nLetters = inputChar - 'A';
+    int nRows = 2 * nLetters + 1;
+    char currentLetter = 'A';
+    int left = nLetters;
+    int right = left;
+    int modifier = 1;
+
+    for (int rowIndex = 0; rowIndex < nRows; rowIndex++) {
+      StringBuilder stringBuilder = new StringBuilder();
+      for (int charIndex = 0; charIndex < nRows; charIndex++) {
+        if ((charIndex == left) || (charIndex == right)) {
+          stringBuilder.append(currentLetter);
+        }
+        else {
+          stringBuilder.append(' ');
+        }
+      }
+      if (rowIndex > nLetters) {
+          modifier = -1;
+      }
+      currentLetter += modifier;
+      right += modifier;
+      left -= modifier;
+      diamondLines.add(stringBuilder.toString());
+    }
+    return diamondLines;
   }
 }
--- diamond/src/test/java/edu/pdx/cs410J/crooney/DiamondTest.java
+++ diamond/src/test/java/edu/pdx/cs410J/crooney/DiamondTest.java
@@ -2,6 +2,8 @@ package edu.pdx.cs410J.crooney;
 
 import org.junit.Test;
 
+import java.util.List;
+
 public class DiamondTest
 {
 
@@ -11,8 +13,25 @@ public class DiamondTest
   }
 
   @Test
-  public void makeDiamondWithAReturnsA() {
-      String output = Diamond.makeDiamond('A');
-      assert(output == "A");
+  public void makeDiamondWithAReturnsOnlyA() {
+      List<String> output = Diamond.makeDiamond('A');
+      assert(output.get(0).equals("A"));
+      assert(output.size() == 1);
+  }
+
+  @Test
+  public void hardCodedCapitalC() {
+      List<String> output = Diamond.makeDiamond('C');
+      assert(output.get(0).equals("  A  "));
+      assert(output.get(1).equals(" B B "));
+      assert(output.get(2).equals("C   C"));
+      assert(output.get(3).equals(" B B "));
+      assert(output.get(4).equals("  A  "));
+  }
+
+  @Test
+  public  void makeDiamondWithBReturnsThreeLines() {
+    List<String> output = Diamond.makeDiamond('B');
+    assert(output.size() == 3);
   }
 }

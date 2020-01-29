2ed36fa5875dbdd2aa09f29e7c3418453cccdc7b
Colleen Rooney <crooney@pdx.edu>
2020-01-29 12:51:01 -0800

    use junit assertEquals

--- diamond/src/test/java/edu/pdx/cs410J/crooney/DiamondTest.java
+++ diamond/src/test/java/edu/pdx/cs410J/crooney/DiamondTest.java
@@ -1,6 +1,7 @@
 package edu.pdx.cs410J.crooney;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 import java.util.List;
 
@@ -8,30 +9,31 @@ public class DiamondTest
 {
 
   @Test
-  public void canInstantiateDiamondClass() {
+  public void canInstantiateDiamondClass()
+ {
     new Diamond();
   }
 
   @Test
   public void makeDiamondWithAReturnsOnlyA() {
       List<String> output = Diamond.makeDiamond('A');
-      assert(output.get(0).equals("A"));
-      assert(output.size() == 1);
+      assertEquals(output.get(0), "A");
+      assertEquals(output.size(), 1);
   }
 
   @Test
   public void hardCodedCapitalC() {
       List<String> output = Diamond.makeDiamond('C');
-      assert(output.get(0).equals("  A  "));
-      assert(output.get(1).equals(" B B "));
-      assert(output.get(2).equals("C   C"));
-      assert(output.get(3).equals(" B B "));
-      assert(output.get(4).equals("  A  "));
+      assertEquals(output.get(0), "  A  ");
+      assertEquals(output.get(1), " B B ");
+      assertEquals(output.get(2), "C   C");
+      assertEquals(output.get(3), " B B ");
+      assertEquals(output.get(4), "  A  ");
   }
 
   @Test
   public  void makeDiamondWithBReturnsThreeLines() {
     List<String> output = Diamond.makeDiamond('B');
-    assert(output.size() == 3);
+    assertEquals(output.size(), 3);
   }
 }
